e0c04f6012e2e2b2283196c935529afb13f19e0b
chidrome <chidrome@gmail.com>
2019-05-03 08:57:21 -0700

    movies and yelp constructor function added

--- server.js
+++ server.js
@@ -35,6 +35,24 @@ const Events = function(link, name, eventDate, summary){
   this.summary = summary;
 };
 
+const Movies = function(title, overview, average_votes, total_votes, image_url, popularity, released){
+  this.title = title;
+  this.overview = overview;
+  this.average_votes = average_votes;
+  this.total_votes = total_votes;
+  this.image_url = image_url;
+  this.popularity = popularity;
+  this.released_on = released;
+};
+
+const Yelp = function(name, image_url, price, rating, url){
+  this.name = name;
+  this.image_url = image_url;
+  this.price = price;
+  this.rating = rating;
+  this.url = url;
+};
+
 // Location Endpoint
 app.get('/location', (request, response) => {
   try {
